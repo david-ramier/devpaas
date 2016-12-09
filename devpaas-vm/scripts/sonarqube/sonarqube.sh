2d04e1d02fb0bf836323b5acc109ffe56f0a4045
Marco Maccio <marco.maccio@marmac.name>
2016-12-09 21:55:47 +0100

    Replace absolute path with relative path based on $HOME variable

--- devpaas-vm/scripts/sonarqube/sonarqube.sh
+++ devpaas-vm/scripts/sonarqube/sonarqube.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash -eux
 
 echo "***** Sonar user creation *****"
 sudo adduser --no-create-home --disabled-login --disabled-password sonar
@@ -11,12 +11,12 @@ echo "***** Configure mysql for sonarqube db *****"
 mysql -h "localhost" < /tmp/sonarqube/resources/sonarqube.sql
 
 echo "***** Download sonarqube *****"
-mkdir /home/packer/sonarqube/
+mkdir $HOME/sonarqube/
 wget https://sonarsource.bintray.com/Distribution/sonarqube/sonarqube-6.1.zip \
-  -O /home/packer/sonarqube/sonarqube-6.1.zip
+  -O $HOME/sonarqube/sonarqube-6.1.zip
 
 sudo mkdir /var/lib/sonarqube
-sudo cp /home/packer/sonarqube/sonarqube-6.1.zip /var/lib/sonarqube
+sudo cp $HOME/sonarqube/sonarqube-6.1.zip /var/lib/sonarqube
 cd /var/lib/sonarqube
 echo "***** Extract Sonarqube *****"
 sudo unzip sonarqube-6.1.zip
