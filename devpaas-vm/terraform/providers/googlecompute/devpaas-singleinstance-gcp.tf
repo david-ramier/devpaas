9b57cde03b1dfa9bddf67ba0b474330017038847
Marco Maccio <marco.maccio@marmac.name>
2017-02-12 21:47:04 +0100

    modified terraform script to instantiate 2 VM on gce

--- devpaas-vm/terraform/providers/googlecompute/devpaas-singleinstance-gcp.tf
+++ devpaas-vm/terraform/providers/googlecompute/devpaas-singleinstance-gcp.tf
@@ -1,7 +1,9 @@
-variable "project_id"           {}
-variable "region"               {}
-variable "machine_type"         {}
-variable "jenkins_image_name"   {}
+variable "project_id"             {}
+variable "region"                 {}
+variable "jenkins_machine_type"   {}
+variable "jenkins_image_name"     {}
+variable "webserver_machine_type" {}
+variable "webserver_image_name"   {}
 
 // Configure the Google Cloud provider
 provider "google" {
@@ -13,9 +15,9 @@ provider "google" {
 // Create a new instance
 resource "google_compute_instance" "jenkins" {
   name         = "mm-devpaas-01"
-  machine_type = "${var.machine_type}"
+  machine_type = "${var.jenkins_machine_type}"
   zone         = "${var.region}"
-  tags 		     = ["devpaas-si"]
+  tags 		   = ["devpaas-si"]
 
   disk {
     image = "${var.jenkins_image_name}"
@@ -29,9 +31,26 @@ resource "google_compute_instance" "jenkins" {
   }
 }
 
-output "mm-jenkins-01" {
-	value = "${google_compute_instance.jenkins.network_interface.access_config.assigned_nat_ip}"
+// Create a new instance
+resource "google_compute_instance" "webserver" {
+  name         = "mm-webserver-01"
+  machine_type = "${var.webserver_machine_type}"
+  zone         = "${var.region}"
+  tags 		   = ["webserver"]
+
+  disk {
+    image = "${var.webserver_image_name}"
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      // Ephemeral IP
+    }
+  }
 }
+
+
 
 resource "google_compute_firewall" "default" {
   name    = "mm-www-firewall"
@@ -39,9 +58,17 @@ resource "google_compute_firewall" "default" {
 
   allow {
     protocol = "tcp"
-    ports    = ["8080", "8081", "9000"]
+    ports    = ["80", "8080", "8081", "9000"]
   }
 
   source_ranges = ["0.0.0.0/0"]
   target_tags   = ["jenkins", "nexus", "sonarqube"]
+}
+
+output "mm-jenkins-01" {
+  value = "${google_compute_instance.jenkins.network_interface.access_config.assigned_nat_ip}"
+}
+
+output "mm-webserver-01" {
+  value = "${google_compute_instance.webserver.network_interface.access_config.assigned_nat_ip}"
 }
