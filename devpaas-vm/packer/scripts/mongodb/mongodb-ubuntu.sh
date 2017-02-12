5effc1766fd362ecf0af32935e4102c84f883642
Marco Maccio <marco.maccio@marmac.name>
2017-02-12 21:44:32 +0100

    Correct script to install nodes and mongodb on ubuntu

--- devpaas-vm/packer/scripts/mongodb/mongodb-ubuntu.sh
+++ devpaas-vm/packer/scripts/mongodb/mongodb-ubuntu.sh
@@ -2,7 +2,7 @@
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 
-echo "deb http://repo.mongodb.org/apt/ubuntu/dists/xenial/mongodb-org/3.4/ multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 
 sudo apt-get -y update
 
