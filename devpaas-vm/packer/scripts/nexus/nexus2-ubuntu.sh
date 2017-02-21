2661f06f0332a6fda12d8586a48cf93b63af85dc
Marco Maccio <marco.maccio@marmac.name>
2017-02-21 15:13:20 +0100

    installation script for both Nexus 2 and Nexus 3

--- devpaas-vm/packer/scripts/nexus/nexus2-ubuntu.sh
+++ devpaas-vm/packer/scripts/nexus/nexus2-ubuntu.sh
@@ -3,19 +3,22 @@
 echo "***** Nexus user creation *****"
 sudo adduser --no-create-home --disabled-login --disabled-password nexus
 
-echo "***** Download Nexus 3 *****"
+echo "***** Download Nexus 2 *****"
 mkdir $HOME/nexus/
+
 wget --no-check-certificate \
- 'https://sonatype-download.global.ssl.fastly.net/nexus/3/nexus-3.1.0-04-unix.tar.gz' \
- -O $HOME/nexus/nexus-3.1.0-04-unix.tar.gz
+ 'https://download.sonatype.com/nexus/oss/nexus-2.14.2-01-bundle.tar.gz' \
+ -O $HOME/nexus/nexus-2.14.2-01-bundle.tar.gz
 
 sudo mkdir /var/lib/sonatype
-sudo cp $HOME/nexus/nexus-3.1.0-04-unix.tar.gz /var/lib/sonatype/
+sudo cp $HOME/nexus/nexus-2.14.2-01-bundle.tar.gz /var/lib/sonatype/
+
 cd /var/lib/sonatype/
-echo "***** Extract Nexus *****"
-sudo tar xvzf nexus-3.1.0-04-unix.tar.gz
-sudo ln -s nexus-3.1.0-04 nexus
-sudo rm -f nexus-3.1.0-04-unix.tar.gz
+
+echo "***** Extract Nexus 2 *****"
+sudo tar xvzf nexus-2.14.2-01-bundle.tar.gz
+sudo ln -s nexus-2.14.2-01 nexus
+sudo rm -f nexus-2.14.2-01-bundle.tar.gz
 
 echo "***** Create some dirs for Nexus *****"
 sudo mkdir -p nexus/bin/jsw/conf/
@@ -29,3 +32,5 @@ sudo cp /tmp/nexus/resources/wrapper.conf         /var/lib/sonatype/nexus/bin/js
 sudo systemctl daemon-reload
 sudo systemctl enable nexus.service
 sudo systemctl start nexus.service
+
+
