586a33a6d2dfc733f491b75e9e8ffaaa9d7ca40f
Marco Maccio <marco.maccio@marmac.name>
2017-02-21 15:23:22 +0100

    Changed the SonarQube MySQL Credentials

--- devpaas-vm/packer/resources/sonarqube/configs/sonarqube.sql
+++ devpaas-vm/packer/resources/sonarqube/configs/sonarqube.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE sonar CHARACTER SET utf8 COLLATE utf8_general_ci;
-CREATE USER 'sonar' IDENTIFIED BY 'sonar';
-GRANT ALL ON sonar.* TO 'sonar'@'%' IDENTIFIED BY 'sonar';
-GRANT ALL ON sonar.* TO 'sonar'@'localhost' IDENTIFIED BY 'sonar';
+CREATE DATABASE sonardb CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE USER 'sonaradmin' IDENTIFIED BY 'sonar123!';
+GRANT ALL ON sonardb.* TO 'sonaradmin'@'%' IDENTIFIED BY 'sonar123!';
+GRANT ALL ON sonardb.* TO 'sonaradmin'@'localhost' IDENTIFIED BY 'sonar123!';
 FLUSH PRIVILEGES;
