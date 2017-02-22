40bef7a7e7cdd67710c160b2cd275f79bd4eda49
Marco Maccio <marco.maccio@marmac.name>
2017-02-22 23:28:10 +0100

    Correct echo message

--- devpaas-vm/packer/scripts/serverspec/serverspec-jenkins.sh
+++ devpaas-vm/packer/scripts/serverspec/serverspec-jenkins.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-echo "***** Running Serverspec Tests x Apache *****"
+echo "***** Copying Serverspec Tests x Jenkins *****"
 
 cp /tmp/jenkins/tests/*.rb   /tmp/serverspec/spec/localhost/
