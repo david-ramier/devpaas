416cf7a4cd1ee6fded03ba0374cef16967491d70
Marco Maccio <marco.maccio@marmac.name>
2017-02-22 23:31:34 +0100

    Correct echo message

--- devpaas-vm/packer/scripts/serverspec/serverspec-sonarqube.sh
+++ devpaas-vm/packer/scripts/serverspec/serverspec-sonarqube.sh
@@ -1,5 +1,5 @@
-#!/bin/bash -e
+#!/bin/bash -eux
 
-echo "***** Running Serverspec Tests x Apache *****"
+echo "***** Copying Serverspec Tests x Sonarqube *****"
 
 cp /tmp/sonarqube/tests/*.rb /tmp/serverspec/spec/localhost/
