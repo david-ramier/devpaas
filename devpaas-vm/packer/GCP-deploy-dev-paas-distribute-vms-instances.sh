37130792bdf95c37472c008e2072a978c78e1108
Marco Maccio <marco.maccio@marmac.name>
2017-04-19 22:04:59 +0200

    Packer launching script refactoring - Made uniform the packer launching scripts used to debug the various packer template for various providers (AWS, GCP, VBOX, etc...)

--- devpaas-vm/packer/GCP-deploy-dev-paas-distribute-vms-instances.sh
+++ devpaas-vm/packer/GCP-deploy-dev-paas-distribute-vms-instances.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # script call example:
-#   ./deploy-in-googlecompute-dev-paas-all-instances.sh [plan|apply|destroy] <gcp_projectid> <gcp_region> <gcp_imageId> <gcp_machine_type>
+#   ./GCP-deploy-dev-paas-distribute-vms-instances.sh [plan|apply|destroy] <gcp_projectid> <gcp_region> <gcp_imageId> <gcp_machine_type>
 # echo $GOOGLE_ACCOUNT_FILE="<path_to_gcloud_json>"  (DEPRECATED)
 # export GOOGLE_CLOUD_KEYFILE_JSON="<path_to_gcloud_json>"
-# ex. ./deploy-in-googlecompute-dev-paas-all-instances.sh plan my-devpaas europe-west1 mm-jenkins-2-v20161102 n1-standard-1
+# ex. ./GCP-deploy-dev-paas-distribute-vms-instances.sh plan my-devpaas europe-west1 mm-jenkins-2-v20161102 n1-standard-1
 
 #TF_VAR_project_id=$1          \
 #TF_VAR_region=$2              \
@@ -12,4 +12,5 @@
 terraform $1  -var "project_id=$2"            \
               -var "region=$3"                \
               -var "jenkins_image_name=$4"    \
-              -var "machine_type=$5"
+              -var "machine_type=$5"          \
+              providers/googlecompute/devpaas-single-vm
