070a77e47205b81b5d56b47850bea4eac1daba04
Marco Maccio <marco.maccio@marmac.name>
2017-06-06 14:27:21 +0200

    Packer new funct. - NGINX added NGINX on Centos templates & rel. scripts

---
+++ devpaas-vm/packer/scripts/nginx/nginx-centos.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+echo "***** Starting NGINX installation"
+
+echo "      Adding NGINX repository "
+sudo yum -y install epel-release
+
+echo "      Install NGINX "
+sudo yum -y install nginx
+
+echo "      Start NGINX  "
+sudo systemctl start nginx
+
+echo "      Opening Firewal ports for http and https "
+#sudo firewall-cmd --permanent --zone=public --add-service=http
+#sudo firewall-cmd --permanent --zone=public --add-service=https
+#sudo firewall-cmd --reload
+
+echo "      Enable NGINX to start at the system boot "
+sudo systemctl enable nginx
