cbea8d89da1f1cc3738d5c03c20bacf7d3888632
Marco Maccio <marco.maccio@marmac.name>
2017-06-06 14:27:21 +0200

    Packer new funct. - NGINX added NGINX on Centos templates & rel. scripts

--- devpaas-vm/packer/resources/nginx/tests/serverspec-nginx_spec.rb
+++ devpaas-vm/packer/resources/nginx/tests/serverspec-nginx_spec.rb
@@ -5,6 +5,11 @@ describe service('nginx'), :if => os[:family] == 'ubuntu' do
   it { should be_running }
 end
 
+describe service('nginx'), :if => os[:family] == 'centos' do
+  it { should be_enabled }
+  it { should be_running }
+end
+
 describe port(80) do
   it { should be_listening }
 end
