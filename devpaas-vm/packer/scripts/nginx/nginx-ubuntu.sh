f258d69fcb71ca8ae1c3309f12ddb48061aea996
Marco Maccio <marco.maccio@marmac.name>
2017-06-24 23:26:04 +0200

    Packer - NGINX - Refactored NGINX image creation for ubuntu-16.04 & rhel-7.3

--- devpaas-vm/packer/scripts/nginx/nginx-ubuntu.sh
+++ devpaas-vm/packer/scripts/nginx/nginx-ubuntu.sh
@@ -1,9 +1,15 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
-echo "***** Starting NGINX installation"
+echo "Starting NGINX installation ..."
 
+echo "***** NGINX installation from apt-get"
 sudo apt-get -y install nginx
 
+echo "***** Deletion of default NGINX configuration"
+cd /etc/nginx/sites-available
+sudo rm default ../sites-enabled/default
+
+echo "***** Replace NGINX configuration with custom configuration"
 sudo cp /tmp/nginx/configs/default.conf /etc/nginx/sites-available/default
 
 sudo nginx -t
