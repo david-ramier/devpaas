99e1adcea6647b812e6b398cf3965e925211c328
Marco Maccio <marco.maccio@marmac.name>
2017-07-13 00:18:32 +0200

    Terraform - Deleted the ubuntu busybox to use the NGINX image

--- devpaas-vm/terraform/providers/amazon-ebs/devpaas-distribute-vms-asg/vpc/instances-test.tf
+++ devpaas-vm/terraform/providers/amazon-ebs/devpaas-distribute-vms-asg/vpc/instances-test.tf
@@ -13,11 +13,13 @@ resource "aws_instance" "mm_devpaas_dv_jumpbox" {
   key_name                = "${var.aws_ssh_key_name}"
   vpc_security_group_ids  = ["${aws_security_group.mm_devpaas_sg_jb.id}"]  // ["${var.sg_jumpbox_id}"]
 
+  /*
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World ! This is the DevPaas Jump Box VM" > index.html
               nohup busybox httpd -f -p 80 &
               EOF
+  */
 
   tags {
     Name = "${var.jumpbox_instance_name}"
