4731deea53e0ebf1902f56c6477e1d7448b3d987
Marco Maccio <marco.maccio@marmac.name>
2017-07-25 22:24:27 +0200

    Packer - NGINX - Simplified the nginx script installation as for the base image is enough the default NGINX configuration

--- devpaas-vm/packer/scripts/nginx/nginx-ubuntu.sh
+++ devpaas-vm/packer/scripts/nginx/nginx-ubuntu.sh
@@ -5,14 +5,8 @@ echo "Starting NGINX installation ..."
 echo "***** NGINX installation from apt-get"
 sudo apt-get -y install nginx
 
-echo "***** Deletion of default NGINX configuration"
-cd /etc/nginx/sites-available
-sudo rm default ../sites-enabled/default
-
-echo "***** Replace NGINX configuration with custom configuration"
-sudo cp /tmp/nginx/configs/default.conf /etc/nginx/sites-available/default
-
+echo "***** Verify NGINX configuration "
 sudo nginx -t
 
+echo "***** Restart NGINX service to verify that restart works"
 sudo systemctl restart nginx
-
