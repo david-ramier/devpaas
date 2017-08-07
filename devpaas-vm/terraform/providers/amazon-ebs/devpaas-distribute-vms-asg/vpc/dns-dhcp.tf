78bf91e3b01f4e91476cd7cddff650fa5f7a06b8
Marco Maccio <marco.maccio@marmac.name>
2017-08-07 15:07:20 +0200

    Terraform - refactor to improve Added definition of Route 53 Private DNS zone for forward and reverse lookup

--- devpaas-vm/terraform/providers/amazon-ebs/devpaas-distribute-vms-asg/vpc/dns-dhcp.tf
+++ devpaas-vm/terraform/providers/amazon-ebs/devpaas-distribute-vms-asg/vpc/dns-dhcp.tf
@@ -1,15 +1,27 @@
-resource "aws_route53_zone" "mm_devpaas_dns_primary" {
+resource "aws_route53_zone" "mm_devpaas_dns_primary_fw" {
   name = "marmac-labs.name"
 
   vpc_id = "${aws_vpc.mm_devpaas_vpc.id}"
+
+  tags {
+    Name = "${var.project_name}-DNS-PRIMARY-FORWARD-ZONE"
+  }
+}
+
+resource "aws_route53_zone" "mm_devpaas_dns_primary_rv" {
+  name = "0.10.in-addr.arpa"
+
+  vpc_id = "${aws_vpc.mm_devpaas_vpc.id}"
+
+  tags {
+    Name = "${var.project_name}-DNS-PRIMARY-REVERSE-ZONE"
+  }
 }
 
 resource "aws_vpc_dhcp_options" "mm_devpaas_vpc_dhcp_options" {
 
-  domain_name = "${aws_route53_zone.mm_devpaas_dns_primary.name}"
+  domain_name = "${aws_route53_zone.mm_devpaas_dns_primary_fw.name}"
   domain_name_servers = ["AmazonProvidedDNS"]
-
-  //domain_name_servers = ["10.0.0.2"]
 
   tags {
     Name = "${var.project_name}-DHCP-OPTIONS"
