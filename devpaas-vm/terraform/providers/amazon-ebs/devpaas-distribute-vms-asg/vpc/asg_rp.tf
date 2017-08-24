f216cb582841c7279aa4e00224ca07fc91ae99f9
Marco Maccio <marco.maccio@marmac.name>
2017-08-24 14:57:15 +0200

    Terraform - refactor to improve Added CloudWatch Metric, ASG policy, ASG Notifications,

--- devpaas-vm/terraform/providers/amazon-ebs/devpaas-distribute-vms-asg/vpc/asg_rp.tf
+++ devpaas-vm/terraform/providers/amazon-ebs/devpaas-distribute-vms-asg/vpc/asg_rp.tf
@@ -59,3 +59,77 @@ resource "aws_autoscaling_group" "mm_devpaas_asg_rp" {
     propagate_at_launch = true
   }
 }
+
+resource "aws_autoscaling_policy" "mm_devpaas_asg_pol_scale_up" {
+  name                    = "revproxy-scale-down"
+  adjustment_type         = "ChangeInCapacity"
+  scaling_adjustment      = "-1"
+  cooldown                = "300"
+  autoscaling_group_name  = "${aws_autoscaling_group.mm_devpaas_asg_rp.name}"
+}
+
+resource "aws_autoscaling_policy" "mm_devpaas_asg_pol_scale_down" {
+  name                    = "revproxy-scale-up"
+  adjustment_type         = "ChangeInCapacity"
+  scaling_adjustment      = "1"
+  cooldown                = "300"
+  autoscaling_group_name  = "${aws_autoscaling_group.mm_devpaas_asg_rp.name}"
+}
+
+resource "aws_autoscaling_notification" "mm_devpaas_asg_notif" {
+
+  group_names   = ["${aws_autoscaling_group.mm_devpaas_asg_rp.name}"]
+
+  notifications = [
+    "autoscaling:EC2_INSTANCE_LAUNCH",
+    "autoscaling:EC2_INSTANCE_TERMINATE",
+    "autoscaling:EC2_INSTANCE_LAUNCH_ERROR",
+    "autoscaling:EC2_INSTANCE_TERMINATE_ERROR"
+  ]
+
+  topic_arn     = "${aws_sns_topic.mm_devpaas_sns_topic_main.arn}"
+}
+
+resource "aws_sns_topic" "mm_devpaas_sns_topic_main" {
+  name = "${var.project_name}-sns-topic"
+
+  provisioner "local-exec" {
+    command = "aws sns subscribe --topic-arn ${self.arn} --protocol email --notification-endpoint ${var.sns_email}"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "mm_devpaas_cma_memory_high" {
+  alarm_name          = "mem-util-high-agents"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "MemoryUtilization"
+  namespace           = "System/Linux"
+  period              = "300"
+  statistic           = "Average"
+  threshold           = "80"
+  alarm_description   = "This metric monitors ec2 memory for high utilization on agent hosts"
+
+  alarm_actions       = ["${aws_autoscaling_policy.mm_devpaas_asg_pol_scale_up.arn}"]
+
+  dimensions {
+    AutoScalingGroupName = "${aws_autoscaling_group.mm_devpaas_asg_rp.name}"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "mm_devpaas_cma_memory_low" {
+  alarm_name = "mem-util-low-agents"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods = "2"
+  metric_name = "MemoryUtilization"
+  namespace = "System/Linux"
+  period = "300"
+  statistic = "Average"
+  threshold = "40"
+  alarm_description = "This metric monitors ec2 memory for low utilization on agent hosts"
+
+  alarm_actions = ["${aws_autoscaling_policy.mm_devpaas_asg_pol_scale_down.arn}"]
+
+  dimensions {
+    AutoScalingGroupName = "${aws_autoscaling_group.mm_devpaas_asg_rp.name}"
+  }
+}
