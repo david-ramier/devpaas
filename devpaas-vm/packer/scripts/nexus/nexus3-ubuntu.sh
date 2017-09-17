f47bee11b6032cea44059555235f97a1229680bc
Marco Maccio <marco.maccio@marmac.name>
2017-09-17 23:15:52 +0200

    Packer - Nexus3 - Added VagrantCloud & deleted cleanup & added aws launc

--- devpaas-vm/packer/scripts/nexus/nexus3-ubuntu.sh
+++ devpaas-vm/packer/scripts/nexus/nexus3-ubuntu.sh
@@ -16,18 +16,29 @@ sudo cp $HOME/nexus/nexus-3.5.1-02-unix.tar.gz /var/lib/sonatype/
 echo "***** Extract Nexus *****"
 cd /var/lib/sonatype/
 sudo tar xvzf nexus-3.5.1-02-unix.tar.gz
-sudo ln -s nexus-3.5.1-02-unix nexus
+
+echo "***** Create Symbolic link for nexus *****"
+sudo ln -s nexus-3.5.1-02 nexus
+
+echo "***** Delete tar file *****"
 sudo rm -f nexus-3.5.1-02-unix.tar.gz
 
-echo "***** Create some dirs for Nexus *****"
-sudo mkdir -p nexus/bin/jsw/conf/
+echo "***** Create some conf dirs for Nexus *****"
+
+sudo mkdir nexus/bin/jsw
+sudo mkdir nexus/bin/jsw/conf/
+
+echo "***** Create working-home for Nexus *****"
 sudo mkdir -p sonatype-work/home/
 sudo chown -R nexus:nexus /var/lib/sonatype
 
-echo '****** Nexus Service Configuration ******'
+echo '****** Configure Nexus Service ******'
 sudo cp /tmp/nexus/resources/nexus.service        /etc/systemd/system/
-sudo cp /tmp/nexus/resources/wrapper.conf         /var/lib/sonatype/nexus/bin/jsw/conf/
+#sudo cp /tmp/nexus/resources/wrapper.conf         /var/lib/sonatype/nexus/bin/jsw/conf/
 
+sudo chown -R nexus:nexus /var/lib/sonatype/nexus/bin/jsw/conf/
+
+echo "***** Reload, Enble & Start Nexus service *****"
 sudo systemctl daemon-reload
 sudo systemctl enable nexus.service
 sudo systemctl start nexus.service
